f80b02a4af06ad907b2f2c92b6d80f2fb38eb4df
Dan Loewenherz <dloewenherz@gmail.com>
2014-03-21 22:25:06 -0500

    initial commit

---
+++ LHSKippt/LHSKippt.h
@@ -0,0 +1,13 @@
+//
+//  LHSKippt.h
+//  LHSKippt
+//
+//  Created by Dan Loewenherz on 3/21/14.
+//  Copyright (c) 2014 Lionheart Software LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LHSKippt : NSObject
+
+@end
---
+++ LHSKippt/LHSKippt.m
@@ -0,0 +1,13 @@
+//
+//  LHSKippt.m
+//  LHSKippt
+//
+//  Created by Dan Loewenherz on 3/21/14.
+//  Copyright (c) 2014 Lionheart Software LLC. All rights reserved.
+//
+
+#import "LHSKippt.h"
+
+@implementation LHSKippt
+
+@end
